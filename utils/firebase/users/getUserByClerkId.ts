87b9749072457fcdb00488125420926983eada3b
Andrés Olivé <andresolive@gmail.com>
2024-07-25 13:21:36 -0600

    create function to get user by clerk id from the db

---
+++ utils/firebase/users/getUserByClerkId.ts
@@ -0,0 +1,21 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '@/utils/firebase/firebaseConfig';
+import { User } from '@/types/User';
+
+export const getUserByClerkId = async (clerkId: string) => {
+  try {
+    const items: User[] = [];
+
+    const q = query(collection(db, 'users'), where('clerkId', '==', clerkId));
+
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      items.push({ ...doc.data() } as unknown as User);
+    });
+
+    return { message: 'Success', data: items[0] };
+  } catch (error) {
+    return { message: 'Error', error };
+  }
+};
