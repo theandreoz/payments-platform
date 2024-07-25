23a5f603aa7143fbf10a3e475cc47a3108ff91b1
Andrés Olivé <andresolive@gmail.com>
2024-07-25 13:21:58 -0600

    create redirect logic in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { auth } from '@clerk/nextjs/server';
+import Link from 'next/link';
+
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
   return (
-    <div>
-      <div>HOME</div>
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
+      <div className="mx-auto w-full max-w-[600px]">
+        <h1 className="mb-4 text-6xl">The Best Payments Platform</h1>
+        <p className="mb-4 text-2xl text-white/60">
+          This is the best app for paying your rent. Period.
+        </p>
+        <div>
+          <Link href={href}>
+            <button className="rounded-lg bg-blue-600 px-4 py-2 text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
