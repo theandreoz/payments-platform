8c57f1562dc3437e33eac340238ab0b5a6bec4e8
Andrés Olivé <andresolive@gmail.com>
2024-07-26 20:40:30 -0600

    display Onboardinf component in new user page

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,13 +1,12 @@
 import { createUser } from '@/firebase/users/createUser';
 import { getUserByClerkId } from '@/firebase/users/getUserByClerkId';
 import { currentUser } from '@clerk/nextjs/server';
+import Onboarding from './components/Onboarding';
 
 const createNewUser = async () => {
   const user = await currentUser();
 
   const match = await getUserByClerkId(user!.id);
-
-  console.log({ match });
 
   if (!match?.data?.clerkId) {
     createUser({
@@ -21,7 +20,11 @@ const createNewUser = async () => {
 
 const NewUser = async () => {
   await createNewUser();
-  return <div>New User</div>;
+  return (
+    <div>
+      <Onboarding />
+    </div>
+  );
 };
 
 export default NewUser;
