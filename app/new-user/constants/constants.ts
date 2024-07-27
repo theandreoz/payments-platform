b3dd602c2a3e97659b0c7e33c8dd93822ec76bee
Andrés Olivé <andresolive@gmail.com>
2024-07-26 20:36:56 -0600

    created onboarding constants

---
+++ app/new-user/constants/constants.ts
@@ -0,0 +1,61 @@
+import GetStarted from '../components/GetStarted';
+import StartUpRental from '../components/StartUpRental';
+import SpecifyAddress from '../components/SpecifyAddress';
+import SpecifyRentAmount from '../components/SpecifyRentAmount';
+import SpecifyPayDate from '../components/SpecifyPayDate';
+import RentalTermsConfirmation from '../components/RentalTermsConfirmation';
+import { ReactNode } from 'react';
+
+const stagesNames = {
+  getStarted: 'getStarted',
+  startUpRental: 'startUpRental',
+  specifyAddress: 'specifyAddress',
+  specifyRentAmount: 'specifyRentAmount',
+  specifyPayDate: 'specifyPayDate',
+  rentalTermsConfirmation: 'rentalTermsConfirmation',
+};
+
+export const stages: { [key: string]: any } = {
+  getStarted: {
+    progress: 0,
+    component: GetStarted,
+    name: stagesNames.getStarted,
+    nextStage: stagesNames.startUpRental,
+    prevStage: null,
+  },
+  startUpRental: {
+    progress: 10,
+    component: StartUpRental,
+    name: stagesNames.startUpRental,
+    nextStage: stagesNames.specifyAddress,
+    prevStage: stagesNames.getStarted,
+  },
+  specifyAddress: {
+    progress: 20,
+    component: SpecifyAddress,
+    name: stagesNames.specifyAddress,
+    nextStage: stagesNames.specifyRentAmount,
+    prevStage: stagesNames.startUpRental,
+  },
+  specifyRentAmount: {
+    progress: 30,
+    component: SpecifyRentAmount,
+    name: stagesNames.specifyRentAmount,
+    nextStage: stagesNames.specifyPayDate,
+    prevStage: stagesNames.specifyAddress,
+  },
+  specifyPayDate: {
+    progress: 40,
+    component: SpecifyPayDate,
+    name: stagesNames.specifyPayDate,
+    nextStage: stagesNames.rentalTermsConfirmation,
+    prevStage: stagesNames.specifyRentAmount,
+  },
+  rentalTermsConfirmation: {
+    progress: 50,
+    component: RentalTermsConfirmation,
+    name: stagesNames.rentalTermsConfirmation,
+    nextStage: null,
+    prevStage: stagesNames.specifyPayDate,
+  },
+};
