89307bdcbce407a2a32c0f70001caa71d0bf78ad
Andrés Olivé <andresolive@gmail.com>
2024-07-26 20:40:03 -0600

    create RentalTermsConfirmation component

---
+++ app/new-user/components/RentalTermsConfirmation.tsx
@@ -0,0 +1,16 @@
+const RentalTermsConfirmation = () => {
+  return (
+    <div className="flex flex-col items-center gap-8 rounded-lg">
+      <div className="flex flex-col gap-1 text-start">
+        <h1 className="text-3xl font-bold text-slate-100">
+          Confirm your rental terms
+        </h1>
+        <p className="text-lg font-normal text-slate-400">
+          Please look this over to make sure all the information is correct.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RentalTermsConfirmation;
