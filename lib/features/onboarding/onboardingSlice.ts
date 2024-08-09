236eea27a2fa5c87aeb8be2cffb41f6326ced61e
Andrés Olivé <andresolive@gmail.com>
2024-08-09 13:58:59 -0600

    add address to onboarding slice

--- lib/features/onboarding/onboardingSlice.ts
+++ lib/features/onboarding/onboardingSlice.ts
@@ -4,10 +4,26 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface OnboardingState {
   propertyType: string;
+  address: {
+    street: string;
+    streetLine2?: string | undefined;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+  };
 }
 
 const initialState = {
   propertyType: 'existingRental',
+  address: {
+    street: '',
+    streetLine2: '',
+    city: '',
+    state: '',
+    zip: '',
+    country: '',
+  },
 };
 
 export const OnboardingSlice = createSlice({
@@ -20,9 +36,22 @@ export const OnboardingSlice = createSlice({
     ) => {
       state.propertyType = action.payload;
     },
+    setAddress: (
+      state: OnboardingState,
+      action: PayloadAction<{
+        street: string;
+        streetLine2: string;
+        city: string;
+        state: string;
+        zip: string;
+        country: string;
+      }>,
+    ) => {
+      state.address = { ...action.payload };
+    },
   },
 });
 
-export const { setPropertyType } = OnboardingSlice.actions;
+export const { setPropertyType, setAddress } = OnboardingSlice.actions;
 
 export default OnboardingSlice.reducer;
