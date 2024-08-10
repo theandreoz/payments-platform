2a5a50b7c3fdc4ce45411d32a55983c30cc0d719
Andrés Olivé <andresolive@gmail.com>
2024-08-10 16:41:55 -0600

    bypass middleware in dev env

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware((auth, req) => {
+  if (process.env.NODE_ENV === 'development') return;
+
   if (!isPublicRoute(req)) auth().protect();
 });
 
