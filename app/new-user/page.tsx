756e048a82bc5b881f9253c400064b8f60481503
Andrés Olivé <andresolive@gmail.com>
2024-08-10 20:09:15 -0600

    create stripe customer on signup

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -2,6 +2,10 @@ import { createUser } from '@/firebase/users/createUser';
 import { getUserByClerkId } from '@/firebase/users/getUserByClerkId';
 import { currentUser } from '@clerk/nextjs/server';
 import Onboarding from './components/Onboarding';
+import {
+  createStripeCustomer,
+  getStripeCustomerByEmail,
+} from '@/utils/api/stripe';
 
 const createNewUser = async () => {
   const user = await currentUser();
@@ -9,11 +13,25 @@ const createNewUser = async () => {
   const match = await getUserByClerkId(user!.id);
 
   if (!match?.data?.clerkId) {
+    const email = user!.emailAddresses[0].emailAddress;
+    const name = `${user!.firstName} ${user!.lastName}`;
+
+    const stripeCustomer = await getStripeCustomerByEmail(email);
+
+    let stripeCustomerId: string;
+    if (!stripeCustomer) {
+      const response = await createStripeCustomer(email, name);
+      stripeCustomerId = response.customerId;
+    } else {
+      stripeCustomerId = stripeCustomer.customer.id;
+    }
+
     createUser({
       clerkId: user!.id,
       email: user!.emailAddresses[0].emailAddress,
       firstName: user!.firstName || '',
       lastName: user!.lastName || '',
+      stripeCustomerId,
     });
   }
 };
