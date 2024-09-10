e451b0767fd66b6271d8fc27714286984096db6b
Andrés Olivé <andresolive@gmail.com>
2024-09-10 14:22:42 -0600

    add /dashboard as redirect url after sign in or sign up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 const SignInPage = () => {
   return (
     <div className="flex h-full w-full items-center justify-center bg-black">
-      <SignIn />
+      <SignIn forceRedirectUrl="/dashboard" />
     </div>
   );
 };
