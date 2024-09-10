d36d8cb0a0dfefc3f68f2212e3f72b1c03f64645
Andrés Olivé <andresolive@gmail.com>
2024-09-10 14:36:46 -0600

    fix redirect url after clerk signup

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 const SignupPage = () => {
   return (
     <div className="flex h-full w-full items-center justify-center bg-black">
-      <SignUp />
+      <SignUp forceRedirectUrl="/new-user" />
     </div>
   );
 };
